ba496f78708d1df869c77d9cf6a9d37ec57c2ce4
rosasjose96 <34173505+rosasjose96@users.noreply.github.com>
2021-08-19 22:07:08 -0500

    add docker and dev profile

--- src/main/java/com/bootcamp/msCredit/models/dto/CustomerDTO.java
+++ src/main/java/com/bootcamp/msCredit/models/dto/CustomerDTO.java
@@ -2,6 +2,7 @@ package com.bootcamp.msCredit.models.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
  */
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 @Builder
 public class CustomerDTO {
     /**
